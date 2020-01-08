f6b9b1952401a50d8ba4627b5e5bd4ddad52b5af
Patrick Gustafson <patrickgus@charter.net>
2020-01-08 08:50:20 -0800

    add GET request for bookmarks by id

--- src/bookmarks/bookmarks-router.js
+++ src/bookmarks/bookmarks-router.js
@@ -49,6 +49,18 @@ bookmarksRouter
       .json(bookmark);
   });
 
+bookmarksRouter
+  .route("/bookmarks/:id")
+  .get((req, res) => {
+    const { id } = req.params;
+    const bookmark = bookmarks.find(b => b.id == id);
 
+    if (!bookmark) {
+      logger.error(`Bookmark with id ${id} not found`);
+      return res.status(404).send("Bookmark not found");
+    }
+
+    res.json(bookmark);
+  });
 
 module.exports = bookmarksRouter;
