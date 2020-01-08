0420e5e596443e6116a34b0c92fa4ee7bd7a9125
Patrick Gustafson <patrickgus@charter.net>
2020-01-08 09:02:42 -0800

    add validation for url and rating

--- src/bookmarks/bookmarks-router.js
+++ src/bookmarks/bookmarks-router.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const uuid = require("uuid/v4");
+const { isWebUri } = require("valid-url");
 const logger = require("../logger");
 const bookmarks = require("../store");
 
@@ -27,6 +28,16 @@ bookmarksRouter
     if (!rating) {
       logger.error(`Rating is required`);
       return res.status(400).send("Invalid data");
+    }
+
+    if (!isWebUri(url)) {
+      logger.error(`Invalid url '${url}' supplied`);
+      return res.status(400).send(`'url' must be a valid URL`);
+    }
+
+    if (!Number.isInteger(rating) || rating < 0 || rating > 5) {
+      logger.error(`Invalid rating '${rating}' supplied`);
+      return res.status(400).send(`'rating' must be a number between 0 and 5`);
     }
 
     const id = uuid();
@@ -65,7 +76,7 @@ bookmarksRouter
   .delete((req, res) => {
     const { id } = req.params;
 
-    const bookmarkIndex = bookmarks.findIndex(b => b.id ==id);
+    const bookmarkIndex = bookmarks.findIndex(b => b.id == id);
 
     if (bookmarkIndex === -1) {
       logger.error(`Bookmark with id ${id} not found`);
