42736573ba66f95a1845f3b51038b9f76007f36f
Patrick Gustafson <patrickgus@charter.net>
2020-01-10 14:58:14 +0000

    add TZ and require dotenv

--- test/setup.js
+++ test/setup.js
@@ -1,5 +1,7 @@
 process.env.NODE_ENV = "test";
 process.env.API_TOKEN = "test-auth-token";
+process.env.TZ = "UTC";
+require("dotenv").config();
 
 const { expect } = require("chai");
 const supertest = require("supertest");
