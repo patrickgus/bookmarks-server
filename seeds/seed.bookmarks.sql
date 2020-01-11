e9101c8deb93736b45dcacc2fd7ca018c6e46ac5
Patrick Gustafson <patrickgus@charter.net>
2020-01-10 16:09:24 -0800

    create seed file

---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,12 @@
+INSERT INTO bookmarks (title, url, description, rating)
+VALUES
+  ('Amazon', 'https://www.amazon.com/', 'Buy literally anything', 4),
+  ('Facebook', 'https://www.facebook.com/', 'Book of faces', 3),
+  ('Google', 'https://www.google.com/', null, 5),
+  ('Thinkful', 'https://www.thinkful.com/', 'Learn to code', 5),
+  ('Github', 'https://github.com/', null, 4),
+  ('LinkedIn', 'https://www.linkedin.com/', 'Social network for your professional network', 4),
+  ('YouTube', 'https://www.youtube.com/', 'Not just cat videos', 3),
+  ('Pandora', 'https://www.pandora.com/', 'Best online radio', 5),
+  ('Twitter', 'https://twitter.com/', 'Look at all the opinions', 2),
+  ('Steam', 'https://store.steampowered.com/', null, 5);
