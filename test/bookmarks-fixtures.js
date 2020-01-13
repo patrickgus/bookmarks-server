abd916b3c3c16a5f9a7136f61da94c33bf6e862b
Patrick Gustafson <patrickgus@charter.net>
2020-01-13 17:14:10 +0000

    remove quotes around rating

--- test/bookmarks-fixtures.js
+++ test/bookmarks-fixtures.js
@@ -5,21 +5,21 @@ function makeBookmarksArray() {
       title: "Amazon",
       url: "https://www.amazon.com/",
       description: "Buy literally anything",
-      rating: "4"
+      rating: 4
     },
     {
       id: 2,
       title: "Facebook",
       url: "https://www.facebook.com/",
       description: "Book of faces",
-      rating: "3"
+      rating: 3
     },
     {
       id: 3,
       title: "Google",
       url: "https://www.google.com/",
       description: "",
-      rating: "5"
+      rating: 5
     }
   ];
 }
